4e3fa70592a7d244344068f0275bf0dc8b065895
aishwarypatil9623 <114222195+aishwarypatil9623@users.noreply.github.com>
2023-05-11 13:37:48 +0530

    Update Array_Based_Stack.py

--- Array_Based_Stack.py
+++ Array_Based_Stack.py
@@ -47,3 +47,22 @@ if __name__ == '__main__':
     print(myStack.isEmpty())                                                    # check if the stack is empty
 
     print(myStack.isFull())                                                     # check if the stack is full
+
+
+'''OUTPUT
+30
+20
+2
+False
+False
+
+Explanation:
+
+myStack.push(10): Pushes the element 10 onto the stack.
+myStack.push(20): Pushes the element 20 onto the stack.
+myStack.push(30): Pushes the element 30 onto the stack.
+print(myStack.pop()): Pops the top element (30) from the stack and prints it.
+print(myStack.peek()): Peeks at the top element (20) of the stack and prints it.
+print(myStack.size()): Returns the current size of the stack (2) and prints it.
+print(myStack.isEmpty()): Returns False since the stack is not empty and prints it.
+print(myStack.isFull()): Returns False since the stack is not full (its capacity is 5 and it currently has 2 elements) and prints it.'''
